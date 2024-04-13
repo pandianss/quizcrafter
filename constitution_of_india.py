8501d821e3964b483821b31f2532bcf20dd5cce7
pandianss <133184149+pandianss@users.noreply.github.com>
2024-04-13 12:13:29 +0530

    New

--- constitution_of_india.py
+++ constitution_of_india.py
@@ -3,21 +3,26 @@ import streamlit as st
 with open('countries.txt', 'r') as file:
     countries = file.readlines()
 
+
 def comment():
     with st.form('Customer feedback form'):
         name_input = st.text_input('Name:', value='')
-        email_input = st.text_input('Valid email id', value='')
-        Country_input = st.selectbox('Country',countries)
-        my_comment = st.text_area('Comment',height=200)
+        email_input = st.text_input('Valid email id:', value='')
+        Country_input = st.selectbox('Country:',countries)
+        my_comment = st.text_area('Comment:',height=200)
         comment_button = st.form_submit_button('Submit comment')
 
 def main():
-    st.set_page_config(page_title='Constition of India')
+    st.set_page_config(page_title='QuizCrafter Series: The Constition of India',page_icon='assets\\color.svg')
+    st.title(':open_book: The Constitution of India')
+    st.caption('Learn all about this living document with the assistance of AI')
+    st.divider()
     st.sidebar.title('Navigation')
     b1 = st.sidebar.button('Learn - Zoom Mode')
     b2 = st.sidebar.button('Quiz')
     b1 = st.sidebar.button('Leaderboard')
     b1 = st.sidebar.button('Analytics')
+
 
     comment()
 
